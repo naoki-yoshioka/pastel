bc7204bf5464ac61cb26794f3cced42152bff050
Naoki Yoshioka <yoshioka.naoki@gmail.com>
2018-01-04 12:23:19 +0900

    Create pastel::particle::point_particle

---
+++ include/pastel/particle/point_particle_io.hpp
@@ -0,0 +1,34 @@
+#ifndef PASTEL_PARTICLE_POINT_PARTICLE_IO_HPP
+# define PASTEL_PARTICLE_POINT_PARTICLE_IO_HPP
+
+# include <cstddef>
+# include <ostream>
+
+# include <pastel/particle/point_particle.hpp>
+# include <pastel/particle/tags.hpp>
+# include <pastel/particle/get.hpp>
+
+
+namespace pastel
+{
+  namespace particle
+  {
+    template <typename Character, typename CharacterTraits, std::size_t dimension_, typename Value>
+    std::basic_ostream<Character, CharacterTraits>& operator<<(
+      std::basic_ostream<Character, CharacterTraits>& output_stream,
+      ::pastel::particle::point_particle<dimension_, Value> const& particle)
+    {
+      return output_stream
+        << '{'
+        << ::pastel::particle::get< ::pastel::particle::tags::position >(particle) << ','
+        << ::pastel::particle::get< ::pastel::particle::tags::velocity >(particle) << ','
+        << ::pastel::particle::get< ::pastel::particle::tags::force >(particle) << ','
+        << ::pastel::particle::get< ::pastel::particle::tags::mass >(particle)
+        << '}';
+    }
+  } // namespace particle
+} // namespace pastel
+
+
+#endif // PASTEL_PARTICLE_POINT_PARTICLE_IO_HPP
+
